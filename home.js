fbe3b67cd379e1d3a805599c2d18f16fcf866573
Claire Yegian <claireyegian@gmail.com>
2022-03-23 09:51:53 -0700

    worked on home page visuals, bio page mockup

---
+++ home.js
@@ -0,0 +1,24 @@
+let actSci = document.querySelector(".actSci");
+actSci.addEventListener("click", () => {
+    window.location = "./actuarialScience.html";
+})
+let bio = document.querySelector(".bio");
+bio.addEventListener("click", () => {
+    window.location = "./biology.html";
+})
+let bioChem = document.querySelector(".bioChem");
+bioChem.addEventListener("click", () => {
+    window.location = "./biochemistry.html";
+})
+let chem = document.querySelector(".chem");
+chem.addEventListener("click", () => {
+    window.location = "./chemistry.html";
+})
+let compSci = document.querySelector(".compSci");
+compSci.addEventListener("click", () => {
+    window.location = "./computerScience.html";
+})
+let econ = document.querySelector(".econ");
+econ.addEventListener("click", () => {
+    window.location = "./economics.html";
+})
